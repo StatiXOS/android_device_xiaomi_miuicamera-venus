1a7b7446bad30d51dec6a6bcbcbe90a9c2bdb95e
Arian <arian.kulmer@web.de>
2025-01-09 18:29:00 +0100

    miuicamera-venus: Patch algo jni lib to use correct Surface::connect method
    In android 15 QPR1, Google has removed a public method of the Surface class.
    Our jni lib used that method and relies on the offset remaining the same, so
    patch the lib to load it with the smaller offset.
    https://android.googlesource.com/platform/frameworks/native/+/6a5fdc1d5f6f76c5938aa73f72941bd3f2643628
    
    Change-Id: I97e7ca9f24adad3ede69b599f8d44fdc96646048
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -32,10 +32,10 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    (
-        'system/lib64/libcamera_algoup_jni.xiaomi.so',
-        'system/lib64/libcamera_mianode_jni.xiaomi.so',
-    ): blob_fixup()
+    'system/lib64/libcamera_algoup_jni.xiaomi.so': blob_fixup()
+        .add_needed('libgui_shim_miuicamera.so')
+        .sig_replace('08 AD 40 F9', '08 A9 40 F9'),
+    'system/lib64/libcamera_mianode_jni.xiaomi.so': blob_fixup()
         .add_needed('libgui_shim_miuicamera.so'),
     'system/lib64/libmicampostproc_client.so': blob_fixup()
         .remove_needed('libhidltransport.so'),
